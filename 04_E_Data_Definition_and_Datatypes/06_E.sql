732df1cb1e1c7ff4f604a4775b6d072f403dc213
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-22 19:24:43 +0300

    06_E_01

--- 04_E_Data_Definition_and_Datatypes/06_E.sql
+++ 04_E_Data_Definition_and_Datatypes/06_E.sql
@@ -1,12 +1,22 @@
 CREATE DATABASE `exercise`;
+
+
 USE `exercise`;
 CREATE TABLE `people` (
 `id` INT PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR(200) NOT NULL,
-`pivture` BLOB,
+`picture` BLOB,
 `height` DOUBLE(10, 2),
 `weight` DOUBLE(10, 2),
 `gender` CHAR(1) NOT NULL,
 `birthdate` DATE NOT NULL,
 `biography` TEXT
 );
+
+USE `exercise`;
+INSERT INTO `people` (`name`, `gender`, `birthdate`)
+	VALUES ("Boris", 'm', DATE(NOW())),
+			("Alexander", 'm', DATE(NOW())),
+            ("Dancho", 'm', DATE(NOW())),
+            ("Peter", 'm', DATE(NOW())),
+            ("Desi", 'f', DATE(NOW()));

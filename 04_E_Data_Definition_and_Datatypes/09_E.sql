fa40a8e5513a1b9c45e6f52409621e3d164cabb9
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-22 21:42:23 +0300

    09_E_03

--- 04_E_Data_Definition_and_Datatypes/09_E.sql
+++ 04_E_Data_Definition_and_Datatypes/09_E.sql
@@ -5,3 +5,6 @@ MODIFY COLUMN `last_login_time` DATETIME DEFAULT NOW();
 
 INSERT INTO `users` (`username`, `password`)
 values ("DestO", "qazxsw");
+
+ALTER TABLE `users`
+MODIFY COLUMN `username` VARCHAR(30) UNIQUE;

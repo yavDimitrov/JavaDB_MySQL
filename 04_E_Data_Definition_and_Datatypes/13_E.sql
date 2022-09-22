99ad3f7994ec532796c73a18a10a8c3a17c66abc
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-22 23:54:28 +0300

    13_e

---
+++ 04_E_Data_Definition_and_Datatypes/13_E.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE `soft_uni`;
+
+USE `soft_uni`;
+CREATE TABLE `towns` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `addresses` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`address_text` VARCHAR(255) NOT NULL,
+`town_id` INT NOT NULL
+ 
+);

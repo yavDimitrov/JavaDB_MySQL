09a807fb30a5283c0e3f6d16c8adcba5c348f298
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-22 19:58:10 +0300

    07_E

---
+++ 04_E_Data_Definition_and_Datatypes/07_E.sql
@@ -0,0 +1,10 @@
+USE `exercise`;
+
+CREATE TABLE `users` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`username` VARCHAR(30) NOT NULL,
+`password` VARCHAR(26) NOT NULL,
+`profile_picture` BLOB,
+`last_login_time` TIME,
+`is_deleted` BOOLEAN
+);

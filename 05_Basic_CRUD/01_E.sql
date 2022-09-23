afb87a24b78b530e8e041d64c61c3def0cbafb08
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-23 23:14:57 +0300

    02_e_02

--- 05_Basic_CRUD/01_E.sql
+++ 05_Basic_CRUD/01_E.sql
@@ -1,5 +1,10 @@
 SELECT
+	`id`,
 	CONCAT_WS(' ', `first_name`, `last_name`) AS 'Full Name',
     `job_title` AS 'Job Title',
     `salary` AS 'Salary'
-    FROM `employees`;
+    FROM `employees`
+    WHERE `salary` > 1000
+    ORDER BY `id`;
+    
+    SELECT DISTINCT `department_id` FROM `employees`;

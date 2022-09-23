660942b8ed146488b68eb36dc42a31e1ec3bb63b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-23 10:04:52 +0300

    13_E

--- 04_E_Data_Definition_and_Datatypes/13_E.sql
+++ 04_E_Data_Definition_and_Datatypes/13_E.sql
@@ -18,3 +18,20 @@ CREATE TABLE `departments` (
     `name` VARCHAR(255) NOT NULL
 );
 
+
+CREATE TABLE `employees`   (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+ `first_name` VARCHAR(255) NOT NULL,
+ `middle_name` VARCHAR(255) NOT NULL,
+ `last_name` VARCHAR(255) NOT NULL,
+ `job_title` VARCHAR(255) NOT NULL,
+ `department_id` INT NOT NULL,
+ `hire_date` DATE,
+ `salary` DECIMAL,
+ `address_id` INT NOT NULL
+ );
+ 
+ 
+ USE `soft_uni`;
+ INSERT INTO `towns` (`name`)
+ VALUES ("Sofia"), ("Plovdiv"), ("Varna"), ("Burgas");

8350b213dd443f457f653a4985248481df3d41a4
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-23 23:44:55 +0300

    05_E_04

---
+++ 05_Basic_CRUD/05_E.sql
@@ -0,0 +1,39 @@
+SELECT
+	`id`,
+	CONCAT_WS(' ', `first_name`, `last_name`) AS 'Full Name',
+    `job_title` AS 'Job Title',
+    `salary` AS 'Salary'
+    FROM `employees`
+    WHERE `salary` > 1000
+    ORDER BY `id`;
+    
+    SELECT `id`, `first_name`, `department_id` 
+    FROM `employees`
+    WHERE `department_id` = 1 OR `department_id` = 2
+    ORDER BY `id`;
+    
+        SELECT `id`, `first_name`, `department_id` 
+    FROM `employees`
+    WHERE `department_id` BETWEEN 1 and 3
+    ORDER BY `id`;      
+    
+    SELECT `id`, `first_name`, `department_id` 
+    FROM `employees`
+    WHERE `department_id` NOT IN ('1', '3')
+    ORDER BY `id`;
+    
+    SELECT * FROM `employees`
+    WHERE `department_id` = 4 AND `salary` > 1000;
+    
+    
+ALTER VIEW `employees_overView` AS
+SELECT
+	`id` AS 'No.',
+	CONCAT_WS(' ', `first_name`, `last_name`) AS 'Full Name',
+    `job_title` AS 'Job Title',
+    `salary` AS 'Salary'
+    FROM `employees`
+    WHERE `salary` > 1000
+    ORDER BY `id`;
+    
+    SELECT * FROM `employees_overView`;

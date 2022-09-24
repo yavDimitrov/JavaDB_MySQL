766af4a2d91047def8e6d7d4c9c054a73c9ddc32
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-24 22:59:19 +0300

    16

--- 06_E_Basic_CRUD/06_Basic_CRUD.sql
+++ 06_E_Basic_CRUD/06_Basic_CRUD.sql
@@ -66,3 +66,11 @@ WHERE `manager_id` IS NULL;
 	FROM `employees`;
     
 SELECT * FROM `v_employees_salaries`;
+
+#16. 1. Create View Employees with Job Titles 
+CREATE VIEW `v_employees_job_titles` AS
+	SELECT CONCAT_WS(' ', `first_name`,`middle_name`, `last_name`) AS `full_name`, `job_title`
+    FROM `employees`;
+    
+SELECT * FROM `v_employees_job_titles`;
+    

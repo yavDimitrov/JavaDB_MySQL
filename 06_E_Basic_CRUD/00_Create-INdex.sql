be50697b2994d4fd9c8a8fccaf1825ec90952b7b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-24 20:08:08 +0300

    13

--- 06_E_Basic_CRUD/00_Create-INdex.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM soft_uni.employees;
-
-CREATE INDEX index1 ON `employees`(`first_name`) ;
-
-SHOW INDEXES FROM `employees`;
--- 06_E_Basic_CRUD/06_Basic_CRUD.sql
+++ 06_E_Basic_CRUD/06_Basic_CRUD.sql
@@ -51,3 +51,7 @@ WHERE `manager_id` IS NULL;
  SELECT `first_name`, `last_name` FROM `employees`
  ORDER BY `salary` DESC
  LIMIT 5;
+ 
+ #13.1. Find All Employees Except Marketing
+ SELECT `first_name`, `last_name` FROM `employees`
+ WHERE `department_id` != 4;

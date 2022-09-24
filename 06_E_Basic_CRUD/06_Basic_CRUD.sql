038ed775414f138992e55e504b7564707e0f768b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-24 17:06:28 +0300

    02

--- 06_E_Basic_CRUD/06_Basic_CRUD.sql
+++ 06_E_Basic_CRUD/06_Basic_CRUD.sql
@@ -1,3 +1,8 @@
 #1. Find All Information About Departments 
 SELECT * FROM `departments`
 ORDER BY `department_id`;
+
+#1. Find all Department Names
+SELECT `name` FROM `departments`
+ORDER BY `department_id`;
+

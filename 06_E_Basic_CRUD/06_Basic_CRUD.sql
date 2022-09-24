cf346a79bc87439bafba2cb8d4d300eff57d8b97
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-24 23:50:43 +0300

    23

--- 06_E_Basic_CRUD/06_Basic_CRUD.sql
+++ 06_E_Basic_CRUD/06_Basic_CRUD.sql
@@ -105,3 +105,10 @@ SELECT `country_name`, `population` FROM `countries`
     ORDER BY `population` DESC, `country_name`
     LIMIT 30;
     
+#23.1. Countries and Currency (Euro / Not Euro) 
+SELECT `country_name`, `country_code`, 
+IF (`currency_code` = 'EUR', 'Euro', 'Not Euro') AS `currency` 
+FROM `countries`
+ORDER BY `country_name`;
+	
+    

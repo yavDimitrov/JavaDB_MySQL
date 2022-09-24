5b94c91685a48ed37fee4f1c3efae2e7fe5c29ee
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-24 18:23:32 +0300

    05

--- 06_E_Basic_CRUD/06_Basic_CRUD.sql
+++ 06_E_Basic_CRUD/06_Basic_CRUD.sql
@@ -13,3 +13,8 @@ ORDER BY `employee_id`;
 #4. Find Full Name of Each Employee
 SELECT `first_name`, `middle_name`, `last_name` FROM `employees`
 ORDER BY `employee_id`;
+
+#5.1. Find Email Address of Each Employee
+SELECT CONCAT(`first_name`, '.', `last_name`, '@softuni.bg')
+AS `full_email_address`
+FROM `employees`;

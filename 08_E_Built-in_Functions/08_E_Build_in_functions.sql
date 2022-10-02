8b84f2c8b1cdde9a93e3edf88d83cfb65f2f3d02
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-02 22:14:25 +0300

    08

--- 08_E_Built-in_Functions/08_E_Build_in_functions.sql
+++ 08_E_Built-in_Functions/08_E_Build_in_functions.sql
@@ -50,5 +50,11 @@
         AND `name` NOT LIKE 'b%'
         AND `name` NOT LIKE 'd%'
         ORDER BY `name`;
+        
+#08. Create View Employees Hired After
+		 CREATE VIEW v_employees_hired_after_2000  AS
+         SELECT `first_name`, `last_name` FROM `employees`
+         WHERE YEAR (`hire_date`) > 2000;
+         SELECT * FROM v_employees_hired_after_2000;
 
 	

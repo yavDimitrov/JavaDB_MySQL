72ea385edbfd7309cc2c2f3bcccdfdb8b45e53fb
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-02 09:21:54 +0300

    02

--- 08_E_Built-in_Functions/08_E_Build_in_functions.sql
+++ 08_E_Built-in_Functions/08_E_Build_in_functions.sql
@@ -3,3 +3,16 @@
 	WHERE `first_name` LIKE 'Sa%' 
 			# WHERE left (e.`first_name`, 2) = 'Sa'
 	ORDER BY `employee_id`;
+    
+#2. Find Names of All Employees by Last Name
+	SELECT `first_name`, `last_name` FROM `employees`
+    WHERE `last_name` LIKE '%ei%'
+    ORDER BY `employee_id`;
+    
+#3. Find First Names of All Employees
+	SELECT `first_name` FROM `employees`
+    WHERE (`department_id`= 3 OR `department_id` = 10)
+	OR
+    YEAR(`hire_date`) BETWEEN 1995 AND 2005
+	ORDER BY `employee_id`;
+    

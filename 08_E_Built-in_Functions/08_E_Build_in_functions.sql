8cc8df2ca9f52e170e07ec64c91605895debac63
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-02 09:23:27 +0300

    03

--- 08_E_Built-in_Functions/08_E_Build_in_functions.sql
+++ 08_E_Built-in_Functions/08_E_Build_in_functions.sql
@@ -10,9 +10,14 @@
     ORDER BY `employee_id`;
     
 #3. Find First Names of All Employees
-	SELECT `first_name` FROM `employees`
+	/*   SELECT `first_name` FROM `employees`
     WHERE (`department_id`= 3 OR `department_id` = 10)
 	OR
     YEAR(`hire_date`) BETWEEN 1995 AND 2005
-	ORDER BY `employee_id`;
+	ORDER BY `employee_id`;  */
     
+	SELECT `first_name` from `employees`
+	WHERE `department_id` IN (3,10)
+	AND 
+	year(`hire_date`) BETWEEN 1995 and 2005
+	ORDER BY `employee_id`;

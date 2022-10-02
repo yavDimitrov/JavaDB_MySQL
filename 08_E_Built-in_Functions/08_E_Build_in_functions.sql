8849ca0fe104c575567c263dddb9e313d01c5300
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-02 09:35:44 +0300

    04

--- 08_E_Built-in_Functions/08_E_Build_in_functions.sql
+++ 08_E_Built-in_Functions/08_E_Build_in_functions.sql
@@ -21,3 +21,8 @@
 	AND 
 	year(`hire_date`) BETWEEN 1995 and 2005
 	ORDER BY `employee_id`;
+    
+#4. Find All Employees Except Engineers
+	SELECT `first_name`, `last_name` FROM `employees`
+    WHERE `job_title` NOT LIKE '%engineer%'
+    ORDER BY `employee_id`;

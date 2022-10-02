6550d1c4cfc0151791908749c08a957f2d61cf60
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-02 23:48:09 +0300

    11

--- 08_E_Built-in_Functions/08_E_Build_in_functions.sql
+++ 08_E_Built-in_Functions/08_E_Build_in_functions.sql
@@ -68,7 +68,20 @@
         WHERE char_length(`last_name`) = 5;
         
 #10. Countries Holding 'A'
-		
+		SELECT `country_name`, `iso_code` FROM `countries`
+        WHERE `country_name` LIKE '%a%a%a%'
+        ORDER BY `iso_code`;
+        
+#11. Mix of Peak and River Names
+		SELECT
+			`peak_name`,
+            `river_name`,
+            CONCAT(LOWER(`peak_name`), SUBSTRING(LOWER(`river_name`), 2))  AS `mix`
+		FROM `peaks`, `rivers`
+        WHERE 
+			RIGHT(`peak_name`, 1) = LEFT(LOWER(`river_name`), 1)
+            ORDER BY `mix`;
+		 
 		
         
 

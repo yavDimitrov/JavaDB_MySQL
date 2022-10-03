103f0ab3a4e76e8d4db2f1c9e1259ca856b05e1d
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-03 22:38:30 +0300

    16

--- 08_E_Built-in_Functions/08_E_Build_in_functions.sql
+++ 08_E_Built-in_Functions/08_E_Build_in_functions.sql
@@ -114,6 +114,15 @@
         END
         AS `Duration`
         FROM `games`;
+        
+	#16. Orders Table
+		SELECT `product_name`,
+			`order_date`,
+            adddate(`order_date`, interval 3 day) AS `pay_due`,
+            adddate(`order_date`, interval 1 month) AS `deliver_due`
+            FROM `orders`;
+		
+
 		 
 		
         

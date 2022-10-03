7ca4c84d207b1eb153766fa2642b6ff290057cbf
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-03 21:51:38 +0300

    14

--- 08_E_Built-in_Functions/08_E_Build_in_functions.sql
+++ 08_E_Built-in_Functions/08_E_Build_in_functions.sql
@@ -93,7 +93,9 @@
         ORDER BY `Email Provider`, `user_name`;
         
 #14. Get Users with IP Address Like Pattern
-		
+		SELECT `user_name`, `ip_address` FROM `users`
+        WHERE `ip_address` LIKE '___.1%.%.___'
+        ORDER BY `user_name`;
 		  
  		
 		 

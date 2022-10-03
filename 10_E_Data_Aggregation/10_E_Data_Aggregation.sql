7e4827f424eade2178a1af5c6c55a0ce309c0e25
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-04 00:44:51 +0300

    02a

--- 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
+++ 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
@@ -5,4 +5,8 @@
 	SELECT MAX(`magic_wand_size`) AS 'longest_magic_wand' FROM `wizzard_deposits`;
     
     #02.A SECOND Longest Magic Wand
-	SELECT MAX(`magic_wand_size`) AS 'longest_magic_wand' FROM `wizzard_deposits`;
+	SELECT DISTINCT `magic_wand_size` AS 'longest_magic_wand' FROM `wizzard_deposits`
+    ORDER BY `magic_wand_size` DESC
+    LIMIT 1 offset 1;
+    
+#03. Longest Magic Wand per Deposit Groups

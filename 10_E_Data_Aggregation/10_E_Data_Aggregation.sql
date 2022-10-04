33efb9bbcd78a8f97368d8abb2239275e2a1b71d
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-04 19:56:18 +0300

    06

--- 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
+++ 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
@@ -24,3 +24,9 @@
 	SELECT `deposit_group`, SUM(`deposit_amount`) AS 'total_sum' FROM `wizzard_deposits`
     GROUP BY `deposit_group`
     ORDER BY `total_sum`;
+    
+#06. Deposits Sum for Ollivander Family
+	SELECT `deposit_group`, SUM(`deposit_amount`) AS 'total_sum' FROM `wizzard_deposits`
+    WHERE `magic_wand_creator` = 'Ollivander family'
+    GROUP BY `deposit_group`
+    order by `deposit_group`;

5648e5e82504810ba104eb76e8fc5c3848cb2df7
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-04 19:39:46 +0300

    04

--- 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
+++ 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
@@ -13,3 +13,9 @@
 	SELECT `deposit_group`, MAX(`magic_wand_size`) AS 'longest_magic_wand' FROM `wizzard_deposits`
     GROUP BY `deposit_group`
     order by `longest_magic_wand`, `deposit_group`;
+    
+#04. Smallest Deposit Group per Magic Wand Size
+	SELECT `deposit_group` FROM `wizzard_deposits`
+    GROUP BY `deposit_group`
+    ORDER BY AVG(`magic_wand_creator`) ASC
+    LIMIT 1;

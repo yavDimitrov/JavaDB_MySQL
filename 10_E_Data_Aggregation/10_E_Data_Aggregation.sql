8fe18f1497dd0c126f45a672fdd4c7e86212ad8a
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-05 20:51:33 +0300

    08

--- 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
+++ 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
@@ -37,5 +37,11 @@
     GROUP BY `deposit_group`
     HAVING `total_sum` < 150000
     ORDER BY `total_sum` DESC;
+    
+#08. Deposit Charge
+	SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS 'min_deposit_charge' FROM `wizzard_deposits`
+    GROUP BY `deposit_group`, `magic_wand_creator`
+    ORDER BY `magic_wand_creator`, `deposit_group`;
+	 
   
 

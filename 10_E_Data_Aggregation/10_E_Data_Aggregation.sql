04b52f84070868a29be132d626feae7fcbd59c01
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-05 22:51:21 +0300

    14

--- 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
+++ 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
@@ -102,7 +102,15 @@
         
         SELECT `department_id`, AVG(`salary`) AS 'avg_salary' FROM `high_paid_employees`
         GROUP BY `department_id`
-        ORDER BY `deparment_id`;
+        ORDER BY `department_id`;
+        
+        
+	#14. Employees Maximum Salaries
+		SELECT `department_id`, MAX(`salary`) AS 'max_salary'  FROM `employees`
+        GROUP BY `department_id`
+        HAVING `max_salary` NOT BETWEEN 30000 AND 70000
+        ORDER BY `department_id`;
+
 		
 		 
 		 

51b71fe40b7a4febb53f3991d7aca997954c15a0
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-05 21:13:46 +0300

    09

--- 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
+++ 10_E_Data_Aggregation/10_E_Data_Aggregation.sql
@@ -42,6 +42,33 @@
 	SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS 'min_deposit_charge' FROM `wizzard_deposits`
     GROUP BY `deposit_group`, `magic_wand_creator`
     ORDER BY `magic_wand_creator`, `deposit_group`;
+    
+#09. Age Groups
+	 /*
+•	[0-10]
+•	[11-20]
+•	[21-30]
+•	[31-40]
+•	[41-50]
+•	[51-60]
+•	[61+] 
+*/
+
+	SELECT (
+    CASE
+		WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
+		WHEN `age` BETWEEN 11 AND 20 THEN '[11-20]'
+		WHEN `age` BETWEEN 21 AND 30 THEN '[21-30]'
+		WHEN `age` BETWEEN 31 AND 40 THEN '[31-40]'
+		WHEN `age` BETWEEN 41 AND 50 THEN '[41-50]'
+		WHEN `age` BETWEEN 51 AND 60 THEN '[51-60]'
+        WHEN `age` >= 61 THEN '[61+]'
+	END
+    ) AS 'age_group', COUNT(`age`) AS 'wizzards_count' FROM `wizzard_deposits`
+     GROUP BY `age_group`
+     ORDER BY `age_group`;
+	 
+	
 	 
   
 

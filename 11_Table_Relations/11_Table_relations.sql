d66d90e06cb77d18858af4bb560e33c5cdb44a56
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-08 09:39:53 +0300

    03a

--- 11_Table_Relations/11_Table_relations.sql
+++ 11_Table_Relations/11_Table_relations.sql
@@ -28,7 +28,18 @@ JOIN
 `campers` AS c ON v.driver_id = c.id;
 
 #3. SoftUni Hiking
-	SELECT * FROM routes,campers;
+	SELECT r.starting_point AS 'route_starting_point',
+			r.end_point AS 'route_ending_point',
+            r.leader_id,
+            CONCAT(c.first_name, ' ', c.last_name) AS 'leader_name'
+            FROM 
+            routes AS r
+			JOIN
+			campers AS c ON r.leader_id = c.id;
+    
+    
+    
+    
 
 
 

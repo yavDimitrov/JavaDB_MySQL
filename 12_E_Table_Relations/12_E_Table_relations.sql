054b7ec37e3811d9b3ecfdd2952be4e83d081666
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-08 19:47:26 +0300

    01.3

--- 12_E_Table_Relations/12_E_Table_relations.sql
+++ 12_E_Table_Relations/12_E_Table_relations.sql
@@ -1,2 +1,25 @@
 #1. One-To-One Relationship
 	CREATE DATABASE table_relations;
+    USE table_relations;
+    
+    CREATE TABLE `passports` (
+    `passport_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `passport_number` VARCHAR(50) UNIQUE
+    );
+    
+    INSERT INTO `passports` (`passport_id`, `passport_number`)
+		VALUES
+			(101, 'N34FG21B'),
+            (102, 'K65LO4R7'),
+            (103, 'ZE657QP2');  
+            
+	CREATE TABLE `people` (
+    `person_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(50),
+    `salary` DECIMAL(9.2),
+    `passport_id` INT UNIQUE,
+    CONSTRAINT fk_people_passport
+    FOREIGN KEY (`passport_id`)
+    REFERENCES `passports` (`passport_id`)
+    );
+    

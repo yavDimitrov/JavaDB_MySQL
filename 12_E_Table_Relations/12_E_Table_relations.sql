46533054d40e5aaaa1ea20dafcdacf4c451929ad
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-08 21:46:10 +0300

    02.01

--- 12_E_Table_Relations/12_E_Table_relations.sql
+++ 12_E_Table_Relations/12_E_Table_relations.sql
@@ -15,8 +15,8 @@
             
 	CREATE TABLE `people` (
     `person_id` INT PRIMARY KEY AUTO_INCREMENT,
-    `first_name` VARCHAR(50),
-    `salary` DECIMAL(9.2),
+    `first_name` VARCHAR(50) NOT NULL,
+    `salary` DECIMAL(9,2),
     `passport_id` INT UNIQUE,
     CONSTRAINT fk_people_passport
     FOREIGN KEY (`passport_id`)
@@ -25,7 +25,20 @@
     
     INSERT INTO `people` (`first_name`, `salary`, `passport_id`)
 		VALUES
-			(`Roberto`, 43300.00, 102),
+			('Roberto', 43300.00, 102),
             ('Tom', 56100.00, 103),
             ('Yana', 60200.00, 101);
+            
+	#02. One-To-Many Relationship
+  CREATE DATABASE `cars`;
+	CREATE TABLE `manufacturers` (
+    `manufacturers_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL UNIQUE,
+    `established_on` DATE
+    );
     
+    INSERT INTO `manufacturers`(`name`, `established_on`)
+		VALUES 
+			('BMW', '1916/03/01'),
+            ('Tesla', '2003/01/01'),
+            ('Lada', '1966/05/01');

ba324d2a96cfd98f3d854dbdfdfdc810a8c98624
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-08 23:27:04 +0300

    03

--- 12_E_Table_Relations/12_E_Table_relations.sql
+++ 12_E_Table_Relations/12_E_Table_relations.sql
@@ -31,14 +31,35 @@
             
 	#02. One-To-Many Relationship
   CREATE DATABASE `cars`;
+  USE `cars`;
 	CREATE TABLE `manufacturers` (
     `manufacturers_id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL UNIQUE,
     `established_on` DATE
-    );
+);
     
     INSERT INTO `manufacturers`(`name`, `established_on`)
 		VALUES 
 			('BMW', '1916/03/01'),
             ('Tesla', '2003/01/01'),
             ('Lada', '1966/05/01');
+            
+	CREATE TABLE `models` (
+    `model_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(70) NOT NULL,
+    `manufacturers_id` INT,
+    CONSTRAINT fk_models_manufacturers 
+    FOREIGN KEY (`manufacturers_id`)
+    REFERENCES `manufacturers`(`manufacturers_id`)
+);
+    
+    INSERT INTO `models` (`model_id`, `name`, `manufacturers_id`)
+		VALUES
+        (101, 'X1', 1),
+        (102, 'i6', 1),
+        (103,'Model S', 2),
+        (104,'Model X', 2),
+        (105, 'Model 3', 2),
+        (106, 'Nova', 3);
+        
+	#03. Many-To-Many Relationship

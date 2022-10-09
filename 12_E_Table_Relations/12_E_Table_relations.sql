a878494bc07a2100059d52399ad48aa8aea28b78
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-09 11:04:15 +0300

    06.01

--- 12_E_Table_Relations/12_E_Table_relations.sql
+++ 12_E_Table_Relations/12_E_Table_relations.sql
@@ -159,7 +159,28 @@
 		`major_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
         `name` VARCHAR(50)
     );
+    
+    CREATE TABLE `payments` (
+		`payment_id` INT PRIMARY KEY AUTO_INCREMENT,
+        `payment_date` DATE NOT NULL,
+        `payment_amount` DECIMAL (8,2),
+        `student_id` INT(11)
+    );
+    
+    CREATE TABLE `students` (
+		`student_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+        `student_number` VARCHAR(12) NOT NULL UNIQUE,
+        `student_name` VARCHAR(50) NOT NULL,
+        `major_id` INT(11)
+     );
 
+
+	CREATE TABLE `agenda` (
+    `student_id` INT NOT NULL,
+    `subject_id` INT NOT NULL,
+    CONSTRAINT pk
+    PRIMARY KEY (`student_id`,`subject_id`)
+    );
     
     
     

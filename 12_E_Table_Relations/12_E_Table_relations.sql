2fa96c786b57b7616cea24384df6e1b6de69055f
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-09 12:32:27 +0300

    9

--- 12_E_Table_Relations/12_E_Table_relations.sql
+++ 12_E_Table_Relations/12_E_Table_relations.sql
@@ -203,6 +203,14 @@
     FOREIGN KEY (`student_id`)
     REFERENCES `students`(`student_id`); 
     
+    #9
+    SELECT m.`mountain_range`, p.`peak_name`, p.`elevation`
+FROM `mountains` AS m
+JOIN `peaks` AS p
+ON p.`mountain_id` = m.`id`
+WHERE m.`mountain_range` = "Rila"
+ORDER BY p.`elevation` DESC;
+    
     
     
     

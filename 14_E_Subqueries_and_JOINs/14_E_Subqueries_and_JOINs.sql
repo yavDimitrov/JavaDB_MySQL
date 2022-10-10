d7e855174cb751a10f1b191633bf00c2e176c0aa
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-10 22:56:39 +0300

    08

--- 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
+++ 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
@@ -46,17 +46,20 @@
     WHERE e.`hire_date` > '1999-01-01 00:00:00' AND d.`name` IN ('Sales', 'Finance')
     ORDER BY e.`hire_date`;
     
+#07. Employees with Project
+    SELECT e.`employee_id`, e.`first_name`, p.`name` AS `project_name`
+	FROM `employees` AS e
+	JOIN `employees_projects` AS ep ON ep.`employee_id` = e.`employee_id`
+	JOIN `projects` AS p ON ep.`project_id` = p.`project_id`
+	WHERE  DATE(p.`start_date`) > '2002-08-13' AND p.`end_date` IS NULL
+	ORDER BY e.`first_name` , p.`name`
+	LIMIT 5;
     
-    SELECT 
-    e.`employee_id`, e.`first_name`, p.`name` AS `project_name`
-FROM
-    `employees` AS e
-        JOIN
-    `employees_projects` AS ep ON ep.`employee_id` = e.`employee_id`
-        JOIN
-    `projects` AS p ON ep.`project_id` = p.`project_id`
-WHERE
-    DATE(p.`start_date`) > '2002-08-13'
-        AND p.`end_date` IS NULL
-ORDER BY e.`first_name` , p.`name`
-LIMIT 5;
+#0.8.Employee 24
+	SELECT e.`employee_id`, e.`first_name`, IF(YEAR(p.`start_date`) >= 2005, NULL, p.`name`) AS 'project_name'
+	FROM `employees` AS e
+	JOIN `employees_projects` AS ep ON ep.`employee_id` = e.`employee_id`
+	JOIN `projects` AS p ON ep.`project_id` = p.`project_id`
+    WHERE e.`employee_id` = 24
+    ORDER BY p.`name`;
+	 

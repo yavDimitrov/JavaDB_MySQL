165ca0b222ae99ea51c72757eb2d48288ec68948
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-10 22:08:23 +0300

    06.1 06.1
    Signed-off-by: Yavor Dimitrov <yav.dimitrov@gmail.com>

--- 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
+++ 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
@@ -40,5 +40,8 @@
 	LIMIT 3;
     
 #06. Employees Hired After
-	 
-    
+	 SELECT e.`first_name`, e.`last_name`, e.`hire_date`, d.`name` AS 'dept_name'
+     FROM `employees` AS e
+    INNER JOIN `departments` AS d ON e.`department_id` = d.`department_id`
+    WHERE e.`hire_date` > '1999-01-01 00:00:00' AND d.`name` IN ('Sales', 'Finance')
+    ORDER BY e.`hire_date`;

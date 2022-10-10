cc60066b6735777e42d217e84cf5df8e5c9eaee7
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-10 08:14:11 +0300

    03

--- 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
+++ 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
@@ -14,3 +14,11 @@
     ORDER BY e.`first_name`, e.`last_name`
     LIMIT 5;
     
+#03. Sales Employee
+	SELECT e.`employee_id`, e.`first_name`, e.`last_name`, d.`name`
+    FROM `employees` AS e
+    INNER JOIN `departments` AS d ON e.`department_id` = d.`department_id`
+    WHERE d.`name` = 'Sales'
+    ORDER BY e.`employee_id` DESC;
+	
+    

fdd6ecd32ee37bc9b01a1abc20b2c61ac451102d
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-10 22:20:08 +0300

    07

--- 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
+++ 14_E_Subqueries_and_JOINs/14_E_Subqueries_and_JOINs.sql
@@ -45,3 +45,18 @@
     INNER JOIN `departments` AS d ON e.`department_id` = d.`department_id`
     WHERE e.`hire_date` > '1999-01-01 00:00:00' AND d.`name` IN ('Sales', 'Finance')
     ORDER BY e.`hire_date`;
+    
+    
+    SELECT 
+    e.`employee_id`, e.`first_name`, p.`name` AS `project_name`
+FROM
+    `employees` AS e
+        JOIN
+    `employees_projects` AS ep ON ep.`employee_id` = e.`employee_id`
+        JOIN
+    `projects` AS p ON ep.`project_id` = p.`project_id`
+WHERE
+    DATE(p.`start_date`) > '2002-08-13'
+        AND p.`end_date` IS NULL
+ORDER BY e.`first_name` , p.`name`
+LIMIT 5;

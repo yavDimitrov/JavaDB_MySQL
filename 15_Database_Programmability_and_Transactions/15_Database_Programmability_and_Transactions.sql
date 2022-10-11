f3c97dad2679f9f7357ae4fd40f75fe86e5eb0e4
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-11 23:23:21 +0300

    15.2

--- 15_Database_Programmability_and_Transactions/15_Database_Programmability_and_Transactions.sql
+++ 15_Database_Programmability_and_Transactions/15_Database_Programmability_and_Transactions.sql
@@ -16,16 +16,27 @@ WHERE `e`.`address_id` IN (
 );
 
 DELIMITER $$
-CREATE FUNCTION ufn_count_employess_by_town()
-RETURNS int
+CREATE FUNCTION ufn_count_employees_by_town(town_name VARCHAR(50))
+RETURNS INT
 DETERMINISTIC
 BEGIN
-	-- DECLARE count_by_town INT;
-    -- SET count_by_town := (SELECT COUNT(*) FROM `employees`);
-    -- RETURN count_by_town;
-    
-    RETURN (SELECT count(*) FROM `employees`);
+	 DECLARE id_for_town INT;
+     DECLARE count_by_town INT;
+     
+     SET id_for_town := (SELECT `town_id` FROM `towns` WHERE `name` = town_name);
+     SET count_by_town := (
+		SELECT COUNT(*) FROM `employees` AS `e`
+			WHERE `e`.`address_id` IN (
+				SELECT `address_id` FROM `addresses` WHERE `town_id` = id_for_town
+            )
+        ); 
+	
+        RETURN count_by_town;
     END$$
+    
+    SELECT `ufn_count_employees_by_town`('Calgary')$$ 
+    
+    DROP FUNCTION `ufn_count_employees_by_town`;
 
 
 

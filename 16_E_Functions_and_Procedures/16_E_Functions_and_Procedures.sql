f61829ce4edef8a753f2a2e4bf9062d00d70edc5
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-14 01:26:10 +0300

    04

--- 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
+++ 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
@@ -31,5 +31,18 @@ BEGIN
 END &&  
 DELIMITER ;
 
+#04
+DELIMITER && 
+CREATE PROCEDURE usp_get_employees_from_town (searched_town VARCHAR(50))
+BEGIN
+  SELECT `first_name`, `last_name`
+  FROM `employees` AS e
+  JOIN `addresses` AS a USING (address_id)
+  JOIN `towns` AS t USING (town_id)
+  WHERE t.`name` = `searched_town`
+  ORDER BY `first_name`,`last_name`;
+END &&  
+DELIMITER ;
+
 
 

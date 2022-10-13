ec1f75934a9f12bd0075bf7de7d4e96b77e09698
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-14 01:25:16 +0300

    02

--- 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
+++ 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
@@ -8,5 +8,16 @@ BEGIN
     ORDER BY `first_name`, `last_name`, `employee_id`;
 END$$
 
+#2. Employees with Salary Above Number
+DELIMITER && 
+CREATE PROCEDURE usp_get_employees_salary_above (desired_salary DECIMAL(19,4))
+BEGIN
+  SELECT `first_name`, `last_name` 
+  FROM `employees`
+  WHERE `salary` >= `desired_salary`
+  order by `first_name`, `last_name`, `employee_id`;
+END &&  
+DELIMITER ;
+
 
 

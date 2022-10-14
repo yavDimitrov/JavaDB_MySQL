dab745767cc9d005711f44f9dc08deacc066d886
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-14 23:22:34 +0300

    07

--- 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
+++ 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
@@ -80,7 +80,17 @@ BEGIN
 END &&
 
 # 07. Define Function
+DELIMITER &&
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters VARCHAR(50), word VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+END &&  
+DELIMITER ;
 
+
+#08. Find Full Name
 
 
 

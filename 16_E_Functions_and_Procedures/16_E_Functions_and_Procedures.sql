5d58acd7b1d4dc73b8697c0918c844325f737f3c
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-14 22:35:11 +0300

    06

--- 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
+++ 16_E_Functions_and_Procedures/16_E_Functions_and_Procedures.sql
@@ -44,5 +44,43 @@ BEGIN
 END &&  
 DELIMITER ;
 
+# 5. Salary Level Function
+DELIMITER && 
+CREATE FUNCTION ufn_get_salary_level(salary DECIMAL(19, 4))
+RETURNS VARCHAR(8)
+DETERMINISTIC
+BEGIN
+    DECLARE salary_level VARCHAR(8);
+    IF salary < 30000 THEN SET salary_level := 'Low';
+    ELSEIF salary <= 50000 THEN SET salary_level := 'Average';
+    #ELSEIF salary BETWEEN(30000 AND 50000) THEN SET salary_level :='average';
+    ELSE SET salary_level := 'High';
+    END IF;
+    RETURN salary_level;
+END &&  
+DELIMITER ;
+
+#06
+DELIMITER &&
+CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(8)) 
+BEGIN
+    SELECT first_name, last_name
+    FROM employees
+    WHERE ufn_get_salary_level(salary) LIKE salary_level
+    ORDER BY first_name DESC, last_name DESC;
+END &&  
+DELIMITER ;
+
+CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(8)) 
+BEGIN
+    SELECT first_name, last_name
+    FROM employees
+    WHERE ufn_get_salary_level(salary) LIKE salary_level
+    ORDER BY first_name DESC, last_name DESC;
+END &&
+
+# 07. Define Function
+
+
 
 

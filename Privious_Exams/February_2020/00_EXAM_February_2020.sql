f026b619d4a7bbf1fbf9a8a4dadd079ef0aacfba
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-16 09:57:25 +0300

    00_February_2020

---
+++ Privious_Exams/February_2020/00_EXAM_February_2020.sql
@@ -0,0 +1,39 @@
+#By error 1140, we can use only grouped by or aggregated colomns in a querry
+
+SELECT @@sql_mode;
+
+SET sql_mode = 'ONLY_FULL_GROUP_BY';
+
+#EXAM
+
+CREATE SCHEMA fsd;
+
+USE fsd;
+
+CREATE TABLE `countries` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE `towns` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(45) NOT NULL,
+`country_id` INT  NOT NULL,
+CONSTRAINT fk_T_countries
+FOREIGN KEY (`country_id`)
+REFERENCES `countries`(`id`)
+);   
+
+CREATE TABLE `stadiums` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(45) NOT NULL,
+`capacity` INT  NOT NULL,
+`town_id` INT  NOT NULL,
+CONSTRAINT fk_stadiums_town
+FOREIGN KEY (`town_id`)
+REFERENCES `towns`(`id`)
+);   
+
+
+
+ 

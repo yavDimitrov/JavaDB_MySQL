032e61def7d220b6604357f9b47db19f7bdea63e
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-16 16:21:52 +0300

    03

--- Privious_Exams/February_2020/00_EXAM_February_2020.sql
+++ Privious_Exams/February_2020/00_EXAM_February_2020.sql
@@ -54,6 +54,47 @@ CREATE TABLE `skills_data` (
 `speed` INT DEFAULT 0,
 `strength` INT DEFAULT 0
 );
+
+CREATE TABLE `coaches` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`first_name` VARCHAR(10) NOT NULL,
+`last_name` VARCHAR(20) NOT NULL,
+`salary` DECIMAL(10,2) NOT NULL DEFAULT 0,
+`coach_level` INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `players_coaches` (
+`player_id` INT ,
+`coach_id` INT,
+CONSTRAINT fk_PK_player
+FOREIGN KEY (`player_id`)
+REFERENCES `players`(`id`),
+
+CONSTRAINT fk_PK_coach
+FOREIGN KEY (`coach_id`)
+REFERENCES `coaches`(`id`)
+);
+
+CREATE TABLE `players` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`first_name` VARCHAR(10) NOT NULL,
+`last_name` VARCHAR(20) NOT NULL,
+`age` INT NOT NULL DEFAULT 0,
+`position` VARCHAR(1) NOT NULL,
+`salary` DECIMAL(10,2) NOT NULL DEFAULT 0,
+`hire_date` DATETIME,
+`skills_data_id` INT NOT NULL,
+`team_id` INT NOT NULL,
+
+CONSTRAINT fk_P_skills_data
+FOREIGN KEY (`skills_data_id`)
+REFERENCES `skills_data`(`id`),
+
+CONSTRAINT fk_P_team
+FOREIGN KEY (`team_id`)
+REFERENCES `teams`(`id`)
+);  
+ 
  
 
 

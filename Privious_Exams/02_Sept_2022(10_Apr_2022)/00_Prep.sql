981db644b21c20880f488938195e6a4512bcf2f8
Yavor Dimitrov <98236929+yavDimitrov@users.noreply.github.com>
2023-07-30 16:44:20 +0300

    DataBAse + EX 02 (Insert)

--- Privious_Exams/02_Sept_2022(10_Apr_2022)/00_Prep.sql
+++ Privious_Exams/02_Sept_2022(10_Apr_2022)/00_Prep.sql
@@ -22,7 +22,9 @@ CREATE TABLE `actors` (
 	`birthdate` DATE NOT NULL,
 	`height` INT,
 	`awards` INT,
-	`country_id` INT NOT NULL
+	`country_id` INT NOT NULL,
+    CONSTRAINT `fk_actors_countries`
+    FOREIGN KEY (`country_id`) REFERENCES `countries`(`id`)
 );
 
 CREATE TABLE `movies_additional_info` (
@@ -40,18 +42,31 @@ CREATE TABLE `movies` (
 `id` INT PRIMARY KEY AUTO_INCREMENT,
 `title` VARCHAR(70) NOT NULL UNIQUE,
 `country_id` INT NOT NULL,
-`movie_info_id` INT NOT NULL UNIQUE
+`movie_info_id` INT NOT NULL UNIQUE,
+CONSTRAINT `fk_movies_countries`
+FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`),
+CONSTRAINT `fk_movies_movies_additional_info`
+FOREIGN KEY (`movie_info_id`) REFERENCES `movies_additional_info`(`id`)
 );
 
 CREATE TABLE `movies_actors` (
 	`movie_id` INT,
 	`actor_id` INT,
-    KEY `pk_movies_actors`(`movie_id` , `actor_id`)
+    # PRIMARY KEY `pk_movies_actors`(`movie_id` , `actor_id`) COMPOSITE PRIMARY KEY UNIQUE INPUTS
+    KEY `pk_movies_actors`(`movie_id` , `actor_id`), # Външни ключове
+    CONSTRAINT `fk_movie_movies_actors`
+    FOREIGN KEY (`movie_id`) REFERENCES `movies`(`id`),
+    CONSTRAINT `fk_actor_movies_actors`
+    FOREIGN KEY (`actor_id`) REFERENCES `actors`(`id`)
 );
 
 CREATE TABLE `genres_movies` (
 	`genre_id` INT,
 	`movie_id` INT,
-    KEY `pk_genres_movies`(`genre_id`, `movie_id`)
+    KEY `pk_genres_movies`(`genre_id`, `movie_id`),
+    CONSTRAINT `fk_genre_genres_movies`
+    FOREIGN KEY (`genre_id`) REFERENCES `genres`(`id`),
+    CONSTRAINT `fk_movie_genres_movies`
+    FOREIGN KEY (`movie_id`) REFERENCES `movies`(`id`)
 
 );
